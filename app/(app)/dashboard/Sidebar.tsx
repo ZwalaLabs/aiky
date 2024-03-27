97b8cc4bb4b4cc7878a31c2fe02d3805bc08e102
Suryansh Anand <spirit@anandsur.com>
2024-03-28 00:22:52 +0530

    feat: dashboard

---
+++ app/(app)/dashboard/Sidebar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { Home, Settings } from "lucide-react";
+import Link from "next/link";
+import { NavigationMenuSub } from "@radix-ui/react-navigation-menu";
+
+function Sidebar() {
+  return (
+    <aside
+      className="fixed left-0 top-[3.5rem] w-72 overflow-y-auto p-4"
+      style={{
+        height: "calc(100vh - 3.5rem)",
+      }}
+    >
+      <NavigationMenu className="flex-col items-start gap-12">
+        <NavigationMenuSub>
+          <NavigationMenuList className="flex-col items-start gap-4">
+            <h3 className="-mb-1 text-xs font-bold text-gray-400/50">HOME</h3>
+            <NavigationMenuItem className="flex items-center gap-2 text-gray-400">
+              <Home />
+              <Link href="#">Home</Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenuSub>
+
+        <NavigationMenuSub>
+          <NavigationMenuList className="flex-col items-start gap-4">
+            <h3 className="-mb-1 text-xs font-bold text-gray-400/50">MANAGE</h3>
+            <NavigationMenuItem className="flex items-center gap-2 text-gray-400">
+              <Settings />
+              <Link href="#">Settings</Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenuSub>
+      </NavigationMenu>
+    </aside>
+  );
+}
+
+export default Sidebar;
--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,5 +1,19 @@
+import Sidebar from "@/app/(app)/dashboard/Sidebar";
+
 function Page() {
-  return <div></div>;
+  return (
+    <>
+      <Sidebar />
+      <main
+        className="ml-80 mt-[3.5rem] flex items-center justify-center px-8"
+        style={{
+          height: "calc(100vh - 3.5rem)",
+        }}
+      >
+        NOpe
+      </main>
+    </>
+  );
 }
 
 export default Page;
