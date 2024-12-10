fc3dd2eef8fae0e5b6503e6c59c5e7628791ee0e
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2024-12-10 23:42:18 +0530

    chaliye shuru karte hai

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -40,10 +40,10 @@ function Landing() {
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-6 text-center">
               <div className="space-y-12 py-4">
-                <h1 className="bg-primary text-3xl font-black tracking-tighter text-white sm:text-4xl md:text-5xl lg:text-8xl">
+                <h1 className="rounded-2xl bg-primary p-3 text-3xl font-black tracking-tighter text-white sm:text-4xl md:text-5xl lg:text-8xl">
                   Monetize your Community
                 </h1>
-                <p className="mx-auto max-w-[700px] pt-3 text-gray-800 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                <p className="mx-auto max-w-[700px] pt-3 text-gray-800 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   The platform for creators to monetize with their audience.
                   <br />
                   Create. Build. Monetize.
@@ -72,7 +72,7 @@ function Landing() {
                 <li>
                   <Card className="w-[450px] px-2 py-4">
                     <div className="grid gap-1">
-                      <h3 className="text-primary text-4xl font-bold">
+                      <h3 className="text-4xl font-bold text-primary">
                         Connect
                       </h3>
                       <p className="text-sm text-gray-500 dark:text-gray-400">
@@ -85,7 +85,7 @@ function Landing() {
                 <li>
                   <Card className="w-[450px] px-2 py-4">
                     <div className="grid gap-1">
-                      <h3 className="text-primary text-4xl font-bold">
+                      <h3 className="text-4xl font-bold text-primary">
                         Exclusive Access
                       </h3>
                       <p className="text-sm text-gray-500 dark:text-gray-400">
@@ -98,7 +98,7 @@ function Landing() {
                 <li>
                   <Card className="w-[450px] px-2 py-4">
                     <div className="grid gap-1">
-                      <h3 className="text-primary text-4xl font-bold">
+                      <h3 className="text-4xl font-bold text-primary">
                         Membership
                       </h3>
                       <p className="text-sm text-gray-500 dark:text-gray-400">
@@ -113,16 +113,16 @@ function Landing() {
         </section>
         <section
           id="about"
-          className="w-full bg-gray-100 py-12 md:py-24 lg:py-32 dark:bg-gray-800"
+          className="w-full bg-gray-100 py-12 dark:bg-gray-800 md:py-24 lg:py-32"
         >
           <div className="container grid items-center gap-6 px-4 md:px-6">
             <div className="space-y-2">
               <h2 className="text-center text-3xl font-bold tracking-tighter md:text-4xl/tight">
                 Discover the Workflow Community Builders Rely On.
               </h2>
-              <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              <p className="mx-auto max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Ready to become part of something bigger? Join{" "}
-                <span className="text-primary font-bold">AIKY</span> today{" "}
+                <span className="font-bold text-primary">AIKY</span> today{" "}
                 <br /> and start building meaningful connections, making a
                 difference, and creating a stronger, more vibrant community.
               </p>
